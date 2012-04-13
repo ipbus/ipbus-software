d93ef0b0062add0fccfe635a1fc6893c48f91841
Robert Frazier <frazier@cern.ch>
2012-04-13 09:20:09 +0000

    Code transfer commit     [CACTUS SVN 51]

---
+++ controlhub/src/ch_stats.erl
@@ -0,0 +1,424 @@
+%%% ===========================================================================
+%%% @author Robert Frazier
+%%%
+%%% @since April 2012
+%%%
+%%% @doc Keeps track of various Control Hub stats such as request/packet
+%%%      counts, etc.
+%%% @end
+%%% ===========================================================================
+-module(ch_stats).
+
+-behaviour(gen_server).
+
+-include("ch_global.hrl").
+
+%% API exports
+-export([start_link/0,
+         stop/0,
+         client_connected/0,
+         client_disconnected/0,
+         client_request_in/0,
+         client_request_malformed/0,
+         client_response_sent/0,
+         udp_in/0,
+         udp_malformed_in/0,
+         udp_out/0,
+         udp_response_timeout/0,
+         get_active_clients/0,
+         get_max_active_clients/0,
+         get_total_client_requests/0,
+         get_total_client_malformed_requests/0,
+         get_total_client_responses/0,
+         get_udp_in/0,
+         get_udp_malformed_in/0,
+         get_udp_out/0,
+         get_udp_response_timeouts/0,
+         report_to_console/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+%% Server state record definition - all of type integer().
+-record(state, {active_clients = 0,
+                max_active_clients = 0,
+                request_count = 0,
+                malformed_request_count = 0,
+                response_count = 0,
+                udp_in = 0,
+                udp_malformed_in = 0,
+                udp_out = 0,
+                udp_response_timeouts = 0}).
+
+
+
+%%% ====================================================================
+%%% API functions
+%%% ====================================================================
+
+    
+%% ---------------------------------------------------------------------
+%% @doc Starts a singleton Control Hub stats server.
+%%
+%% @spec start_link() -> {ok, Pid} | {error, {already_started, Pid}}
+%% where
+%%   Pid = pid()
+%% @end
+%% ---------------------------------------------------------------------
+start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Stops the Control Hub stats server.
+%%
+%% @spec stop() -> ok
+%% @end
+%% ---------------------------------------------------------------------
+stop() -> gen_server:cast(?MODULE, stop).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Inform the stats server that a user-client has connected.
+%%
+%% @spec client_connected() -> ok
+%% @end
+%% ---------------------------------------------------------------------
+client_connected() -> gen_server:cast(?MODULE, client_connected).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Inform the stats server that a user-client has disconnected.
+%%
+%% @spec client_disconnected() -> ok
+%% @end
+%% ---------------------------------------------------------------------
+client_disconnected() -> gen_server:cast(?MODULE, client_disconnected).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Inform the stats server that a user-client request has arrived.
+%%      Note: this is for registering that a user-client request of
+%%      some type - valid or malformed - has arrived at the Control Hub.
+%%
+%% @spec client_request_in() -> ok
+%% @end
+%% ---------------------------------------------------------------------
+client_request_in() -> gen_server:cast(?MODULE, client_request_in).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Inform the stats server that a user-client request was determined
+%%      to be malformed.
+%%
+%% @spec client_request_malformed() -> ok
+%% @end
+%% ---------------------------------------------------------------------
+client_request_malformed() -> gen_server:cast(?MODULE, client_request_malformed).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Inform the stats server that a response has been sent to a
+%%      user-client by the Control Hub.
+%%
+%% @spec client_response_sent() -> ok
+%% @end
+%% ---------------------------------------------------------------------
+client_response_sent() -> gen_server:cast(?MODULE, client_response_sent).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Inform the stats server that a UDP packet arrived.
+%%
+%% @spec udp_in() -> ok
+%% @end
+%% ---------------------------------------------------------------------
+udp_in() -> gen_server:cast(?MODULE, udp_in).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Inform the stats server that a UDP packet arrived and was
+%%      determined to be malformed.
+%%
+%% @spec udp_malformed_in() -> ok
+%% @end
+%% ---------------------------------------------------------------------
+udp_malformed_in() -> gen_server:cast(?MODULE, udp_malformed_in).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Inform the stats server that a UDP packet was sent.
+%%
+%% @spec udp_out() -> ok
+%% @end
+%% ---------------------------------------------------------------------
+udp_out() -> gen_server:cast(?MODULE, udp_out).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Inform the stats server that a UDP response timeout occurred.
+%%
+%% @spec udp_response_timeout() -> ok
+%% @end
+%% ---------------------------------------------------------------------
+udp_response_timeout() -> gen_server:cast(?MODULE, udp_response_timeout).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Returns the current number of connected user-clients.
+%%
+%% @spec get_active_clients() -> integer()
+%% @end
+%% ---------------------------------------------------------------------
+get_active_clients() -> gen_server:call(?MODULE, get_active_clients).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Returns the highest ever value of connected user-clients.
+%%
+%% @spec get_max_active_clients() -> integer()
+%% @end
+%% ---------------------------------------------------------------------
+get_max_active_clients() -> gen_server:call(?MODULE, get_max_active_clients).
+
+
+%% ---------------------------------------------------------------------
+%% @doc Returns the total number of client requests that have been
+%%      received. Note that this total does not differentiate between
+%%      valid/malformed requests, it's just the total number of requests
+%%      that have arrived at the Control Hub.
+%%
+%% @spec get_total_client_requests() -> integer()
+%% @end
+%% ---------------------------------------------------------------------
+get_total_client_requests() -> gen_server:call(?MODULE, get_total_client_requests).
+
+
+%% ----------------------------------------------------------------------------
+%% @doc Returns the total number of client requests that have arrived but have
+%%      been determined to be malformed.
+%%
+%% @spec get_total_client_malformed_requests() -> integer()
+%% @end
+%% ----------------------------------------------------------------------------
+get_total_client_malformed_requests() -> gen_server:call(?MODULE, get_total_client_malformed_requests).
+
+
+%% ----------------------------------------------------------------------------
+%% @doc Returns the total number of responses that have been sent to user
+%%      clients by the Control Hub.
+%%
+%% @spec get_total_client_responses() -> integer()
+%% @end
+%% ----------------------------------------------------------------------------
+get_total_client_responses() -> gen_server:call(?MODULE, get_total_client_responses).
+
+
+%% ----------------------------------------------------------------------------
+%% @doc Returns the current total of incoming UDP packets that have been
+%%      received (malformed or valid).
+%%
+%% @spec get_udp_in() -> integer()
+%% @end
+%% ----------------------------------------------------------------------------
+get_udp_in() -> gen_server:call(?MODULE, get_udp_in).
+
+
+%% ----------------------------------------------------------------------------
+%% @doc Returns the current total of incoming UDP packets that have been
+%%      determined to be malformed.
+%%
+%% @spec get_udp_malformed_in() -> integer()
+%% @end
+%% ----------------------------------------------------------------------------
+get_udp_malformed_in() -> gen_server:call(?MODULE, get_udp_malformed_in).
+
+%% ----------------------------------------------------------------------------
+%% @doc Returns the current total of UDP packets that have been sent.
+%%
+%% @spec get_udp_out() -> integer()
+%% @end
+%% ----------------------------------------------------------------------------
+get_udp_out() -> gen_server:call(?MODULE, get_udp_out).
+
+%% ----------------------------------------------------------------------------
+%% @doc Returns the current total of UDP reponse timeouts that have occurred.
+%%
+%% @spec get_udp_response_timeouts() -> integer()
+%% @end
+%% ----------------------------------------------------------------------------
+get_udp_response_timeouts() -> gen_server:call(?MODULE, get_udp_response_timeouts).
+
+
+%% ----------------------------------------------------------------------------
+%% @doc Prints a report of all the current stats to the console.
+%%
+%% @spec get_all_stats() -> ok
+%% @end
+%% ----------------------------------------------------------------------------
+report_to_console() -> gen_server:cast(?MODULE, report_to_console).
+
+
+
+%%% ====================================================================
+%%% gen_server callbacks
+%%% ====================================================================
+
+
+%% --------------------------------------------------------------------
+%% Function: init/1
+%% Description: Initiates the server
+%% Returns: {ok, State}          |
+%%          {ok, State, Timeout} |
+%%          ignore               |
+%%          {stop, Reason}
+%% --------------------------------------------------------------------
+init([]) ->
+    {ok, #state{}}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_call/3
+%% Description: Handling call messages
+%% Returns: {reply, Reply, State}          |
+%%          {reply, Reply, State, Timeout} |
+%%          {noreply, State}               |
+%%          {noreply, State, Timeout}      |
+%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_call(get_active_clients, _From,  State) ->
+    {reply, State#state.active_clients, State};
+
+handle_call(get_max_active_clients, _From,  State) ->
+    {reply, State#state.max_active_clients, State};
+
+handle_call(get_total_client_requests, _From,  State) ->
+    {reply, State#state.request_count, State};
+
+handle_call(get_total_client_malformed_requests, _From,  State) ->
+    {reply, State#state.malformed_request_count, State};
+
+handle_call(get_total_client_responses, _From,  State) ->
+    {reply, State#state.response_count, State};
+
+handle_call(get_udp_in, _From,  State) ->
+    {reply, State#state.udp_in, State};
+
+handle_call(get_udp_malformed_in, _From,  State) ->
+    {reply, State#state.udp_malformed_in, State};
+
+handle_call(get_udp_out, _From,  State) ->
+    {reply, State#state.udp_out, State};
+
+handle_call(get_udp_response_timeouts, _From,  State) ->
+    {reply, State#state.udp_response_timeouts, State};
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+
+%% --------------------------------------------------------------------
+%% Function: handle_cast/2
+%% Description: Handling cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_cast(client_connected, State = #state{active_clients = Clients, max_active_clients = MaxClients}) ->
+    NewClients = Clients+1,
+    NewMaxClients = if
+                        MaxClients < NewClients -> NewClients;
+                        true -> MaxClients
+                    end,
+    NewState = State#state{active_clients = NewClients,
+                           max_active_clients = NewMaxClients},
+    {noreply, NewState};
+
+handle_cast(client_disconnected, State = #state{active_clients = Clients}) ->
+    NewState = State#state{active_clients = Clients-1},
+    {noreply, NewState};
+
+handle_cast(client_request_in, State = #state{request_count = Requests}) ->
+    NewState = State#state{request_count = Requests+1},
+    {noreply, NewState};
+
+handle_cast(client_request_malformed, State = #state{malformed_request_count = BadRequests}) ->
+    NewState = State#state{malformed_request_count = BadRequests+1},
+    {noreply, NewState};
+
+handle_cast(client_response_sent, State = #state{response_count = Responses}) ->
+    NewState = State#state{response_count = Responses+1},
+    {noreply, NewState};
+
+handle_cast(udp_in, State = #state{udp_in = Packets}) ->
+    NewState = State#state{udp_in = Packets+1},
+    {noreply, NewState};
+
+handle_cast(udp_malformed_in, State = #state{udp_malformed_in = Packets}) ->
+    NewState = State#state{udp_malformed_in = Packets+1},
+    {noreply, NewState};
+
+handle_cast(udp_out, State = #state{udp_out = Packets}) ->
+    NewState = State#state{udp_out = Packets+1},
+    {noreply, NewState};
+
+handle_cast(udp_response_timeout, State = #state{udp_response_timeouts = Value}) ->
+    NewState = State#state{udp_response_timeouts = Value+1},
+    {noreply, NewState};
+
+handle_cast(report_to_console, State) ->
+    io:format("Control Hub Stats Report~n"
+              "------------------------~n~n"
+              "CLIENT  Active connections: ~p (peak=~p)~n"
+              "         Requests received: ~p (of which ~p were malformed)~n"
+              "            Responses sent: ~p~n~n"
+              "UDP             Packets In: ~p (of which ~p were malformed)~n"
+              "               Packets Out: ~p~n"
+              "                  Timeouts: ~p~n",
+              [State#state.active_clients,
+               State#state.max_active_clients,
+               State#state.request_count,
+               State#state.malformed_request_count,
+               State#state.response_count,
+               State#state.udp_in,
+               State#state.udp_malformed_in,
+               State#state.udp_out,
+               State#state.udp_response_timeouts]),
+    {noreply, State};
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+
+%% --------------------------------------------------------------------
+%% Function: handle_info/2
+%% Description: Handling all non call/cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+
+%% --------------------------------------------------------------------
+%% Function: terminate/2
+%% Description: Shutdown the server
+%% Returns: any (ignored by gen_server)
+%% --------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+
+%% --------------------------------------------------------------------
+%% Func: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%% --------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+%%% --------------------------------------------------------------------
+%%% Internal functions
+%%% --------------------------------------------------------------------
+
