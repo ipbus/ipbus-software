5bbb2b7e47778d309a4dc9ec67ec9eaf1341dbb2
Andrew William Rose <andrew.william.rose@cern.ch>
2012-06-08 13:53:15 +0000

    Correction to last commit     [CACTUS SVN 173]

--- uhal/uhal/src/common/ClientFactory.cpp
+++ uhal/uhal/src/common/ClientFactory.cpp
@@ -13,30 +13,32 @@ namespace uhal
 			{
 				mInstance = new ClientFactory();
 				// ---------------------------------------------------------------------
-				/*				mInstance->add< uhal::IPBusUDPClient<IPbus_1_3> > ( "ipbusudp" , "Direct access to hardware via UDP, using the default IPbus version which is currently IPbus 1.3" );
-								mInstance->add< uhal::IPBusUDPClient<IPbus_1_2> > ( "ipbusudp-1.2" );*/
+				mInstance->add< uhal::IPBusUDPClient<IPbus_1_3> > ( "ipbusudp" , "Direct access to hardware via UDP, using the default IPbus version which is currently IPbus 1.3" );
+				// mInstance->add< uhal::IPBusUDPClient<IPbus_1_2> > ( "ipbusudp-1.2" );
 				mInstance->add< uhal::IPBusUDPClient<IPbus_1_3> > ( "ipbusudp-1.3" );
-				/*				// mInstance->add< uhal::IPBusUDPClient<IPbus_1_4> > ( "ipbusudp-1.4" );
-								// mInstance->add< uhal::IPBusUDPClient<IPbus_2_0> > ( "ipbusudp-2.0" );
-								// ---------------------------------------------------------------------
-								mInstance->add< uhal::IPBusTCPClient<IPbus_1_3> > ( "ipbustcp" , "Direct access to hardware via TCP, using the default IPbus version which is currently IPbus 1.3" );*/
-				mInstance->add< uhal::IPBusTCPClient<IPbus_1_3> > ( "ipbustcp-1.3" );
-				/*				// mInstance->add< uhal::IPBusTCPClient<IPbus_1_4> > ( "ipbustcp-1.4" );
-								// mInstance->add< uhal::IPBusTCPClient<IPbus_2_0> > ( "ipbustcp-2.0" );
-								// ---------------------------------------------------------------------
-								mInstance->add< uhal::ControlHubClient<CHH_1 , IPbus_1_3> > ( "chtcp" , "Hardware access via the Control Hub, using Control Hub Protocol 1 (Multi target packets with 32-bit identifier) and the default IPbus version which is currently IPbus 1.3" );
-								mInstance->add< uhal::ControlHubClient<CHH_1 , IPbus_1_2> > ( "chtcp-1.2" );
-								mInstance->add< uhal::ControlHubClient<CHH_1 , IPbus_1_3> > ( "chtcp-1.3" );
-								// mInstance->add< uhal::ControlHubClient<CHH_1 , IPbus_1_4> > ( "chtcp-1.4" );
-								// mInstance->add< uhal::ControlHubClient<CHH_1 , IPbus_2_0> > ( "chtcp-2.0" );
-								// ---------------------------------------------------------------------
-								mInstance->add< uhal::ControlHubClient<CHH_2 , IPbus_1_3> > ( "chtcp2" , "Hardware access via the Control Hub, using Control Hub Protocol 2 (Multi target packets with 48-bit IP+port as identifier) and the default IPbus version which is currently IPbus 1.3" );
-								mInstance->add< uhal::ControlHubClient<CHH_2 , IPbus_1_3> > ( "chtcp2-1.3" );
-								// mInstance->add< uhal::ControlHubClient<CHH_2 , IPbus_1_4> > ( "chtcp2-1.4" );
-								// mInstance->add< uhal::ControlHubClient<CHH_2 , IPbus_2_0> > ( "chtcp2-2.0" );*/
+				// mInstance->add< uhal::IPBusUDPClient<IPbus_1_4> > ( "ipbusudp-1.4" );
+				// mInstance->add< uhal::IPBusUDPClient<IPbus_2_0> > ( "ipbusudp-2.0" );
 				// ---------------------------------------------------------------------
-				// mInstance->add< uhal::ControlHubClient<IPbus_1_3> > ( "chtcp3" , "Hardware access via the Control Hub, using Control Hub Protocol 3 (Single target packets with 48-bit IP+port as identifier) and the default IPbus version which is currently IPbus 1.3" );
-				mInstance->add< uhal::ControlHubClient<IPbus_1_3> > ( "chtcp3-1.3" );
+				mInstance->add< uhal::IPBusTCPClient<IPbus_1_3> > ( "ipbustcp" , "Direct access to hardware via TCP, using the default IPbus version which is currently IPbus 1.3" );
+				mInstance->add< uhal::IPBusTCPClient<IPbus_1_3> > ( "ipbustcp-1.3" );
+				// mInstance->add< uhal::IPBusTCPClient<IPbus_1_4> > ( "ipbustcp-1.4" );
+				// mInstance->add< uhal::IPBusTCPClient<IPbus_2_0> > ( "ipbustcp-2.0" );
+				// ---------------------------------------------------------------------
+				/*
+					mInstance->add< uhal::ControlHubClient<CHH_1 , IPbus_1_3> > ( "chtcp" , "Hardware access via the Control Hub, using Control Hub Protocol 1 (Multi target packets with 32-bit identifier) and the default IPbus version which is currently IPbus 1.3" );
+					mInstance->add< uhal::ControlHubClient<CHH_1 , IPbus_1_2> > ( "chtcp-1.2" );
+					mInstance->add< uhal::ControlHubClient<CHH_1 , IPbus_1_3> > ( "chtcp-1.3" );
+					// mInstance->add< uhal::ControlHubClient<CHH_1 , IPbus_1_4> > ( "chtcp-1.4" );
+					// mInstance->add< uhal::ControlHubClient<CHH_1 , IPbus_2_0> > ( "chtcp-2.0" );
+					// ---------------------------------------------------------------------
+					mInstance->add< uhal::ControlHubClient<CHH_2 , IPbus_1_3> > ( "chtcp2" , "Hardware access via the Control Hub, using Control Hub Protocol 2 (Multi target packets with 48-bit IP+port as identifier) and the default IPbus version which is currently IPbus 1.3" );
+					mInstance->add< uhal::ControlHubClient<CHH_2 , IPbus_1_3> > ( "chtcp2-1.3" );
+					// mInstance->add< uhal::ControlHubClient<CHH_2 , IPbus_1_4> > ( "chtcp2-1.4" );
+					// mInstance->add< uhal::ControlHubClient<CHH_2 , IPbus_2_0> > ( "chtcp2-2.0" );
+				*/
+				// ---------------------------------------------------------------------
+				mInstance->add< uhal::ControlHubClient<IPbus_1_3> > ( "chtcp" , "Hardware access via the Control Hub, using the default IPbus version which is currently IPbus 1.3" );
+				mInstance->add< uhal::ControlHubClient<IPbus_1_3> > ( "chtcp-1.3" );
 				// mInstance->add< uhal::ControlHubClient<CHH_3 , IPbus_1_4> > ( "chtcp2-1.4" );
 				// mInstance->add< uhal::ControlHubClient<CHH_3 , IPbus_2_0> > ( "chtcp2-2.0" );
 				// ---------------------------------------------------------------------
