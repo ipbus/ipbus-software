8271a667841e78f6ede7bd54bc9d5a535f6123a0
Andrew William Rose <andrew.william.rose@cern.ch>
2012-06-25 09:27:16 +0000

    Update tutorial     [CACTUS SVN 254]

--- uhal/tutorials/tutorial_2/src/common/tutorial_2.cxx
+++ uhal/tutorials/tutorial_2/src/common/tutorial_2.cxx
@@ -15,6 +15,13 @@ int main ( int argc , char** argv )
 		ConnectionManager lConnectionManager ( "     file://~/uhal/tutorials/tutorial_2/addr*/*connections.xml   ; file://~/connection*.xml  ;  ;;; ; ;  " );
 		ConnectionManager lConnectionManager2 ( " http://svnweb.cern.ch/world/wsvn/cactus/trunk/uhal/tutorials/tutorial_2/addr/connections.xml?op=dl&rev=head      " );
 				
+		std::vector<std::string> lDeviceIds = lConnectionManager.getDevices ();
+
+		for ( std::vector<std::string>::const_iterator lIt = lDeviceIds.begin() ; lIt != lDeviceIds.end(); ++lIt )
+		{
+			log( Info() , "All Device IDs included string " , Quote( *lIt ) );
+		}
+
 		std::vector<std::string> lRegexDeviceIds = lConnectionManager.getDevices ( "sys1.crate1.*" );
 
 		for ( std::vector<std::string>::const_iterator lIt = lRegexDeviceIds.begin() ; lIt != lRegexDeviceIds.end(); ++lIt )
