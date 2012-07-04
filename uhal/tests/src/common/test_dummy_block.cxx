4f7c1d8ff05c350979b81f89d6804e0da598935b
Marc Magrans De Abril <Marc.Magrans.De.Abril@cern.ch>
2012-07-04 15:09:23 +0000

    #TRAC-19: * block write and read test     [CACTUS SVN 319]

---
+++ uhal/tests/src/common/test_dummy_block.cxx
@@ -0,0 +1,62 @@
+#include "uhal/uhal.hpp"
+
+#include "uhal/tests/tools.hpp"
+
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <typeinfo>
+
+using namespace uhal;
+
+#define N_SMALL   1024/4
+#define N_MEDIUM  1024*1024/4
+#define N_LARGE   1024*1024*1024/4
+
+void block_write_read(size_t N,const std::string& connection, const std::string& id) {
+  ConnectionManager manager(connection);
+  
+  HwInterface hw=manager.getDevice(id);
+  
+  std::vector<uint32_t> xx;
+  for(size_t i=0; i!= N; ++i)
+    xx.push_back(static_cast<uint32_t>(rand()));
+	    
+  hw.getNode("MEM").writeBlock(xx);
+  ValVector< uint32_t > mem = hw.getNode("MEM").readBlock(N);
+  
+  CACTUS_CHECK(!mem.valid());
+  CACTUS_TEST_THROW(mem.size(),uhal::exception);
+  CACTUS_TEST_THROW(mem.at(0),uhal::exception);
+  //CACTUS_TEST_THROW(mem.size(),uhal::NonValidatedMemory); --> https://svnweb.cern.ch/trac/cactus/ticket/32
+  
+  CACTUS_TEST(hw.dispatch());
+  
+  CACTUS_CHECK(mem.valid());
+  CACTUS_CHECK(mem.size() == N);
+
+  bool correct_block_write_read = true;
+  ValVector< uint32_t >::const_iterator i=mem.begin();
+  std::vector< uint32_t >::const_iterator j=xx.begin();
+  for(ValVector< uint32_t >::const_iterator i(mem.begin());i!=mem.end();++i)
+    correct_block_write_read = correct_block_write_read && (*i == *j);
+  
+  CACTUS_CHECK(correct_block_write_read);
+  CACTUS_TEST_NOTHROW(mem.at(0));
+  CACTUS_TEST_NOTHROW(mem.at(N-1));
+}
+
+int main ( int argc,char* argv[] )
+{
+  std::map<std::string,std::string> params = tests::default_arg_parsing(argc,argv);
+  
+  std::string connection_file = params["connection_file"];
+  std::string device_id = params["device_id"];
+  std::cout << "STARTING TEST " << argv[0] << " (connection_file='" << connection_file<<"', device_id='" << device_id << "')..." << std::endl;
+  
+  CACTUS_TEST(block_write_read(N_SMALL,connection_file,device_id));
+  CACTUS_TEST(block_write_read(N_MEDIUM,connection_file,device_id));
+  CACTUS_TEST(block_write_read(N_LARGE,connection_file,device_id));
+  
+  return 0;
+}
--- uhal/tests/src/common/test_dummy_check_permissions.cxx
+++ uhal/tests/src/common/test_dummy_check_permissions.cxx
@@ -8,28 +8,6 @@
 
 using namespace uhal;
 
-void connect_write_read(const std::string& connection, const std::string& id) {
-  ConnectionManager manager(connection);
-
-  HwInterface hw=manager.getDevice(id);
-  
-  hw.ping();
-
-  uint32_t x = static_cast<uint32_t>(rand());
-  
-  hw.getNode("REG").write(x);
-  ValWord< uint32_t > mem = hw.getNode("REG").read();
-  
-  CACTUS_CHECK(!mem.valid());
-  CACTUS_TEST_THROW(mem.value(),uhal::exception);
-  //CACTUS_TEST_THROW(mem.value(),uhal::NonValidatedMemory); --> https://svnweb.cern.ch/trac/cactus/ticket/32
-  
-  CACTUS_TEST(hw.dispatch());
-  
-  CACTUS_CHECK(mem.valid());
-  CACTUS_CHECK(mem.value() == x);
-}
-
 void check_permissions(const std::string& connection, const std::string& id) {
   ConnectionManager manager(connection);
   HwInterface hw=manager.getDevice(id);
@@ -62,7 +40,6 @@ int main ( int argc,char* argv[] )
   std::string device_id = params["device_id"];
   std::cout << "STARTING TEST " << argv[0] << " (connection_file='" << connection_file<<"', device_id='" << device_id << "')..." << std::endl;
   
-  CACTUS_TEST(connect_write_read(connection_file,device_id));
   CACTUS_TEST(check_permissions(connection_file,device_id));
   
   return 0;
