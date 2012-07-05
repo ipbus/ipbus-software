2009b59512479d97e60b22e0c13f2aefb5ac29ae
Andrew William Rose <andrew.william.rose@cern.ch>
2012-07-05 11:11:32 +0000

    Correct test (https://svnweb.cern.ch/trac/cactus/ticket/37)     [CACTUS SVN 326]

--- uhal/tests/src/common/test_dummy_masking.cxx
+++ uhal/tests/src/common/test_dummy_masking.cxx
@@ -20,23 +20,36 @@ void write_read_masked(const std::string& connection, const std::string& id) {
   uint32_t x2 = static_cast<uint32_t>(rand());
 
   hw.getNode("REG_WITHOUT_MASK").write(x1);
-  ValWord< uint32_t > mem1 = hw.getNode("REG_WITH_MASK").read();
+  ValWord< uint32_t > mem1 = hw.getNode("REG_WITHOUT_MASK").read();
+  ValWord< uint32_t > mem2 = hw.getNode("REG_WITH_MASK").read();
 
   hw.getNode("REG_WITH_MASK").write(x2);
-  ValWord< uint32_t > mem2 = hw.getNode("REG_WITHOUT_MASK").read();
-
-  CACTUS_CHECK(!mem1.valid() && !mem2.valid());
+  ValWord< uint32_t > mem3 = hw.getNode("REG_WITHOUT_MASK").read();
+  ValWord< uint32_t > mem4 = hw.getNode("REG_WITH_MASK").read();
+  
+  CACTUS_CHECK(!mem1.valid() && !mem2.valid() && !mem3.valid() && !mem4.valid() );
   CACTUS_TEST_THROW(mem1.value(),uhal::exception);
   CACTUS_TEST_THROW(mem2.value(),uhal::exception);
+  CACTUS_TEST_THROW(mem3.value(),uhal::exception);
+  CACTUS_TEST_THROW(mem4.value(),uhal::exception);  
   //CACTUS_TEST_THROW(mem.value(),uhal::NonValidatedMemory); --> https://svnweb.cern.ch/trac/cactus/ticket/32
   
-  CACTUS_TEST(hw.dispatch());
+  CACTUS_TEST(hw.dispatch()); 
+  CACTUS_CHECK(mem1.valid() && mem2.valid() && mem3.valid() && mem4.valid() );
+
+  std::cout << std::setfill('0') << std::hex;
+  std::cout << "Written first, unmasked : x1 = 0x" << std::setw(8) << x1 << std::endl;
+  std::cout << "Read back, unmasked : mem1 = 0x" << std::setw(8) << mem1.value() << std::endl;
+  std::cout << "Read back, masked : mem2 = 0x" << std::setw(8) << mem2.value() << std::endl;
+  std::cout << "Written second, masked with 0xFFFF and left shifted by 8 bits : x2 = 0x" << std::setw(8) << x2 << std::endl;
+  std::cout << "Read back, unmasked : mem3 = 0x" << std::setw(8) << mem3.value() << std::endl;
+  std::cout << "Read back, masked : mem4 = 0x" << std::setw(8) << mem4.value() << std::endl;
+  std::cout << std::dec;
   
-  CACTUS_CHECK(mem1.valid() && mem2.valid());
-  std::cout << "x1=" << std::hex << x1 << ", mem1=" << mem1.value() << std::dec << std::endl;
-  std::cout << "x2=" << std::hex << x2 << ", mem1=" << mem2.value() << std::dec << std::endl;
-  CACTUS_CHECK(mem1.value() == ((x1 >> 8 ) & 0xffff) );
-  CACTUS_CHECK(mem2.value() == ((mem1.value() | 0xff0000ff) | ((x2 >> 8 ) & 0xffff) << 8) );
+  CACTUS_CHECK( mem1.value() == x1 );
+  CACTUS_CHECK( mem2.value() == ( ( x1 >> 8 ) & 0xffff ) );
+  CACTUS_CHECK( mem3.value() == ( ( x1 & 0xff0000ff ) | ( ( x2 & 0xffff ) << 8 ) ) );
+  CACTUS_CHECK( mem4.value() == ( x2 & 0xffff ) );
 }
 
 int main ( int argc,char* argv[] )
