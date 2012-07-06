e2e312ae881148cff6b4b4276171957c5c653929
Marc Magrans De Abril <Marc.Magrans.De.Abril@cern.ch>
2012-07-06 15:07:14 +0000

    #TRAC-33: * Add on the fly test     [CACTUS SVN 346]

--- uhal/tests/src/common/test_dummy_single.cxx
+++ uhal/tests/src/common/test_dummy_single.cxx
@@ -2,6 +2,8 @@
 
 #include "uhal/tests/tools.hpp"
 
+#include <boost/filesystem.hpp> 
+
 #include <iostream>
 #include <cstdlib>
 #include <typeinfo>
@@ -24,12 +26,48 @@ void connect_write_read ( const std::string& connection, const std::string& id )
 	CACTUS_CHECK ( mem.value() == x );
 }
 
+void on_the_fly_connect_write_read(const std::string& connection, const std::string& id) {
+  //get location of address file. Assumption: it is located with the connection file
+  std::string address_file;
+  {
+    boost::filesystem::path conn_fn(connection);
+    boost::filesystem::path fn("dummy_address.xml");
+    address_file = (conn_fn.parent_path()/fn).string();
+  }
+
+  //get the parameters from the file
+  std::string uri;
+  {
+    ConnectionManager manager(connection);
+    uri = manager.getDevice(id).uri();
+  }
+  
+  HwInterface hw=ConnectionManager::getDevice("test_device_id",uri,address_file);
+
+  uint32_t x = static_cast<uint32_t>(rand());
+  hw.getNode("REG").write(x);
+  ValWord< uint32_t > mem = hw.getNode("REG").read();
+
+  CACTUS_CHECK(!mem.valid());
+  CACTUS_TEST_THROW(mem.value(),uhal::exception);
+
+  CACTUS_TEST(hw.dispatch());
+  
+  CACTUS_CHECK(mem.valid());
+  CACTUS_CHECK(mem.value() == x);
+    
+}
+
 int main ( int argc,char* argv[] )
 {
-	std::map<std::string,std::string> params = tests::default_arg_parsing ( argc,argv );
-	std::string connection_file = params["connection_file"];
-	std::string device_id = params["device_id"];
-	std::cout << "STARTING TEST " << argv[0] << " (connection_file='" << connection_file<<"', device_id='" << device_id << "')..." << std::endl;
-	CACTUS_TEST ( connect_write_read ( connection_file,device_id ) );
-	return 0;
+  std::map<std::string,std::string> params = tests::default_arg_parsing(argc,argv);
+  
+  std::string connection_file = params["connection_file"];
+  std::string device_id = params["device_id"];
+  std::cout << "STARTING TEST " << argv[0] << " (connection_file='" << connection_file<<"', device_id='" << device_id << "')..." << std::endl;
+  
+  CACTUS_TEST(connect_write_read(connection_file,device_id));
+  CACTUS_TEST(on_the_fly_connect_write_read(connection_file,device_id));
+
+  return 0;
 }
