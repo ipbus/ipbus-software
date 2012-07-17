115e40e88d5fda4ba468b8540fb76ad6add89de8
Marc Magrans De Abril <Marc.Magrans.De.Abril@cern.ch>
2012-07-17 07:56:19 +0000

    enable second level     [CACTUS SVN 368]

--- uhal/tests/src/common/test_dummy_block.cxx
+++ uhal/tests/src/common/test_dummy_block.cxx
@@ -26,11 +26,14 @@ void block_write_read ( size_t N,const std::string& connection, const std::strin
 
 	hw.getNode ( "MEM" ).writeBlock ( xx );
 	ValVector< uint32_t > mem = hw.getNode ( "MEM" ).readBlock ( N );
+
 	CACTUS_CHECK ( !mem.valid() );
 	CACTUS_CHECK ( mem.size() == N );
 	CACTUS_TEST_THROW ( mem.at ( 0 ),uhal::exception );
 	//CACTUS_TEST_THROW(mem.size(),uhal::NonValidatedMemory); --> https://svnweb.cern.ch/trac/cactus/ticket/32
+
 	CACTUS_TEST ( hw.dispatch() );
+
 	CACTUS_CHECK ( mem.valid() );
 	CACTUS_CHECK ( mem.size() == N );
 	bool correct_block_write_read = true;
@@ -83,10 +86,12 @@ int main ( int argc,char* argv[] )
 	std::string connection_file = params["connection_file"];
 	std::string device_id = params["device_id"];
 	std::cout << "STARTING TEST " << argv[0] << " (connection_file='" << connection_file<<"', device_id='" << device_id << "')..." << std::endl;
+
 	CACTUS_TEST ( block_write_read ( N_1kB,connection_file,device_id ) );
 	CACTUS_TEST ( block_write_read ( N_100kB,connection_file,device_id ) );
 	CACTUS_TEST ( block_write_read ( N_10MB,connection_file,device_id ) );
 	//1GB hangs the test PC...swapping
+
 	CACTUS_TEST ( fifo_write_read ( N_1kB,connection_file,device_id ) );
 	CACTUS_TEST ( fifo_write_read ( N_100kB,connection_file,device_id ) );
 	CACTUS_TEST ( fifo_write_read ( N_10MB,connection_file,device_id ) );
