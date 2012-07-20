c04e9a16ec1e55e98d3ec97b703ce7df7e9df986
Robert Frazier <frazier@cern.ch>
2012-07-20 15:09:44 +0000

    Given that any one uHAL instance now opens a socket per device, I have upped the max TCP connections to 256.     [CACTUS SVN 382]

--- controlhub/include/ch_tcp_server_params.hrl
+++ controlhub/include/ch_tcp_server_params.hrl
@@ -12,7 +12,7 @@
 -define(CONTROL_HUB_TCP_LISTEN_PORT, 10203).
 
 % The maximum number of TCP user-client connections we will offer.
--define(MAX_CONCURRENT_CLIENT_CONNECTIONS, 64).
+-define(MAX_CONCURRENT_CLIENT_CONNECTIONS, 256).
 
 % The various TCP socket options the ControlHub uses
 % Note that {packet, 4} implies that there is a 4-byte, big-endian "logical stream-size header" at
