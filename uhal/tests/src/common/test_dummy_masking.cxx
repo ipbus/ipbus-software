bfd77d4b350dab3dde09311f1bc353d1d88048b0
Marc Magrans De Abril <Marc.Magrans.De.Abril@cern.ch>
2012-10-26 08:31:15 +0000

    test if masking in memeoris failed & clean up     [CACTUS SVN 587]

--- uhal/tests/src/common/test_dummy_masking.cxx
+++ uhal/tests/src/common/test_dummy_masking.cxx
@@ -33,13 +33,14 @@ void write_read_masked ( const std::string& connection, const std::string& id )
   CACTUS_CHECK ( reg_u.value() == ( x >> 16 ) );
 }
 
+
+
 int main ( int argc,char* argv[] )
 {
   std::map<std::string,std::string> params = tests::default_arg_parsing ( argc,argv );
   std::string connection_file = params["connection_file"];
   std::string device_id = params["device_id"];
   std::cout << "STARTING TEST " << argv[0] << " (connection_file='" << connection_file<<"', device_id='" << device_id << "')..." << std::endl;
-  CACTUS_TEST ( write_read_masked ( connection_file,device_id ) );
   CACTUS_TEST ( write_read_masked ( connection_file,device_id ) );
   return 0;
 }
