2f682d514c0b2493710c98ea3606652bf7f159ef
Andrew William Rose <andrew.william.rose@cern.ch>
2012-12-22 11:51:43 +0000

    Add gpl licensing note to files and a copy of the gpl license file     [CACTUS SVN 650]

--- uhal/uhal/src/common/ClientInterface.cpp
+++ uhal/uhal/src/common/ClientInterface.cpp
@@ -1,3 +1,35 @@
+/*
+---------------------------------------------------------------------------
+
+    This file is part of uHAL.
+
+    uHAL is a hardware access library and programming framework
+    originally developed for upgrades of the Level-1 trigger of the CMS
+    experiment at CERN.
+
+    uHAL is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    uHAL is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with uHAL.  If not, see <http://www.gnu.org/licenses/>.
+
+
+      Andrew Rose, Imperial College, London
+      email: awr01 <AT> imperial.ac.uk
+
+      Marc Magrans de Abril, CERN
+      email: marc.magrans.de.abril <AT> cern.ch
+
+---------------------------------------------------------------------------
+*/
+
 #include "uhal/ClientInterface.hpp"
 
 #include "uhal/Utilities.hpp"
