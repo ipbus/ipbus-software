cebe3ff1371f38e4b6daa65cb673220ebbd41d77
Marc Magrans De Abril <Marc.Magrans.De.Abril@cern.ch>
2013-01-07 10:54:43 +0000

    added check after the timeout     [CACTUS SVN 658]

--- uhal/tests/src/common/test_dummy_timeout.cxx
+++ uhal/tests/src/common/test_dummy_timeout.cxx
@@ -51,30 +51,16 @@ void check_timeout ( const std::string& connection, const std::string& id, int s
   std::cout << "Sleeping for " << sleepAfterFirstDispatch << " seconds to allow DummyHardware to clear itself" << std::endl;
   sleep ( sleepAfterFirstDispatch );
   // Check we can continue as normal without further exceptions.
-  CACTUS_TEST_NOTHROW ( { hw.getNode ( "REG" ).read();  hw.dispatch(); } );
+  
+  uint32_t x = static_cast<uint32_t>(rand());
+  ValWord<uint32_t> y;
+  CACTUS_TEST_NOTHROW (
+		       hw.getNode("REG").write(x); 
+		       y = hw.getNode ( "REG" ).read();  
+		       hw.dispatch();
+		       );
+  CACTUS_CHECK(x==y);
 }
-
-/*
-void check_timeout_setting ( const std::string& connection, const std::string& id, int sleepAfterFirstDispatch )
-{
-  ConnectionManager manager ( connection );
-  HwInterface hw = manager.getDevice ( id );
-  std::cout << "Checking timeout set correctly : Default value" << std::endl;
-  hw.getNode ( "REG" ).read();
-  log ( Info() , ThisLocation() , " : mTimeOut = " , Integer ( hw.getClient().getTimeoutPeriod() ) );
-  hw.dispatch();
-  std::cout << "Checking timeout set correctly : Set to 10ms" << std::endl;
-  hw.getClient().setTimeoutPeriod ( 10 );
-  hw.getNode ( "REG" ).read();
-  log ( Info() , ThisLocation() , " : mTimeOut = " , Integer ( hw.getClient().getTimeoutPeriod() ) );
-  hw.dispatch();
-  std::cout << "Checking timeout set correctly : Set to pos_infinity" << std::endl;
-  hw.getClient().setTimeoutPeriod ( 0 );
-  hw.getNode ( "REG" ).read();
-  log ( Info() , ThisLocation() , " : mTimeOut = " , Integer ( hw.getClient().getTimeoutPeriod() ) );
-  hw.dispatch();
-}
-*/
 
 int main ( int argc,char* argv[] )
 {
@@ -83,6 +69,6 @@ int main ( int argc,char* argv[] )
   std::string device_id = params["device_id"];
   std::cout << "STARTING TEST " << argv[0] << " (connection_file='" << connection_file<<"', device_id='" << device_id << "')..." << std::endl;
   CACTUS_TEST ( check_timeout ( connection_file, device_id, 3 ) );
-  //check_timeout_setting ( connection_file, device_id, 3 );
+
   return 0;
 }
