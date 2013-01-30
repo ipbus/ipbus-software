3e7a06340fe473a6ccdf45090e9bd785d0195917
Tom Williams <tom.williams@cern.ch>
2013-01-30 12:05:04 +0000

    controlhub : Correct unittest code for yesterday's changes to `transaction_manager` (plus small amount of cleanup).     [CACTUS SVN 5723]

--- controlhub/src/unittest/ch_transaction_manager_tests.erl
+++ controlhub/src/unittest/ch_transaction_manager_tests.erl
@@ -96,32 +96,32 @@ test_normal_operation_multi_req_single_target(TestBaggage) ->
     DummyIPbusRequest1 = << 16#01010101:32 >>,
     DummyIPbusRequest2 = << 16#02020202:32, 16#03030303:32 >>,
     DummyIPbusRequest3 = << 16#04040404:32, 16#05050505:32, 16#06060606:32 >>,    
-    % Build the test request
-    TestRequest = << ?LOCALHOST:32,
-                     (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest1) div 4):16,
-                     DummyIPbusRequest1/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest2) div 4):16,
-                     DummyIPbusRequest2/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest3) div 4):16,
-                     DummyIPbusRequest3/binary >>,
+    % Build the test request packets
+    TestRequests = [<< ?LOCALHOST:32,
+                      (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest1) div 4):16,
+                      DummyIPbusRequest1/binary >>,
+                    << ?LOCALHOST:32,
+                      (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest2) div 4):16,
+                      DummyIPbusRequest2/binary >>,
+                    << ?LOCALHOST:32,
+                       (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest3) div 4):16,
+                       DummyIPbusRequest3/binary >>],
     % The response we expect back
-    ExpectedResponse = << (size(DummyIPbusRequest1) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest1/binary,
-                          (size(DummyIPbusRequest2) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest2/binary,
-                          (size(DummyIPbusRequest3) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest3/binary >>,
+    ExpectedResponses = [<< (size(DummyIPbusRequest1) +8):32,
+                            ?LOCALHOST:32,
+                            (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
+                            DummyIPbusRequest1/binary >>,
+                         << (size(DummyIPbusRequest2) +8):32,
+                            ?LOCALHOST:32,
+                            (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
+                            DummyIPbusRequest2/binary >>,
+                         << (size(DummyIPbusRequest3) +8):32,
+                            ?LOCALHOST:32,
+                            (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
+                            DummyIPbusRequest3/binary >>],
     % Run the test
-    ReceivedResponse = create_send_receive(TestBaggage#test_baggage.listen_socket, TestRequest),
-    ?assertEqual(ExpectedResponse, ReceivedResponse).
+    ReceivedResponses = create_send_receive(TestBaggage#test_baggage.listen_socket, TestRequests),
+    ?assertEqual(ExpectedResponses, ReceivedResponses).
 
 
 %% Tests the sending of the same request to multiple target devices
@@ -131,114 +131,61 @@ test_normal_operation_same_req_multi_targets(TestBaggage) ->
                            16#cafebabe:32,
                            16#a5a5a5a5:32,
                            16#69696969:32 >>,
-    % Build the test request
-    TestRequest = << ?LOCALHOST:32,
-                     (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest) div 4):16,
-                     DummyIPbusRequest/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(2, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest) div 4):16,
-                     DummyIPbusRequest/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(3, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest) div 4):16,
-                     DummyIPbusRequest/binary >>,
-    % The response we expect back
-    ExpectedResponse = << (size(DummyIPbusRequest) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest/binary,
-                          (size(DummyIPbusRequest) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(2, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest/binary,
-                          (size(DummyIPbusRequest) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(3, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest/binary >>,
+    % Build the test requests
+    TestRequests = [<< ?LOCALHOST:32,
+                       (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest) div 4):16,
+                       DummyIPbusRequest/binary >>,
+                    << ?LOCALHOST:32,
+                       (lists:nth(2, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest) div 4):16,
+                       DummyIPbusRequest/binary >>,
+                    << ?LOCALHOST:32,
+                       (lists:nth(3, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest) div 4):16,
+                       DummyIPbusRequest/binary >>],
+    % The responses we expect back
+    ExpectedResponses = [<< (size(DummyIPbusRequest) +8):32,
+                            ?LOCALHOST:32,
+                            (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
+                            DummyIPbusRequest/binary >>,
+                         << (size(DummyIPbusRequest) +8):32,
+                            ?LOCALHOST:32,
+                            (lists:nth(2, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
+                            DummyIPbusRequest/binary >>,
+                         << (size(DummyIPbusRequest) +8):32,
+                            ?LOCALHOST:32,
+                            (lists:nth(3, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
+                            DummyIPbusRequest/binary >>],
     % Run the test
-    ReceivedResponse = create_send_receive(TestBaggage#test_baggage.listen_socket, TestRequest),
-    ?assertEqual(ExpectedResponse, ReceivedResponse).
+    ReceivedResponses = create_send_receive(TestBaggage#test_baggage.listen_socket, TestRequests),
+    ?assertEqual(ExpectedResponses, ReceivedResponses).
 
 
-%% Tests the sending multiple different requests to multiple target devices
+%% Tests the sending multiple different requests to multiple target devices (from one TCP client)
 test_normal_operation_multi_req_multi_targets(TestBaggage) ->
-    % The dummy content of our three requests - only going to echo server, so no need for real IPbus content.
-    DummyIPbusRequest1_1 = << 16#01010001:32 >>,
-    DummyIPbusRequest1_2 = << 16#01020001:32, 16#01020002:32 >>,
-    DummyIPbusRequest1_3 = << 16#01030001:32, 16#01030002:32, 16#01030003:32 >>,
-    DummyIPbusRequest2_1 = << 16#02010001:32 >>,
-    DummyIPbusRequest2_2 = << 16#02020001:32, 16#02020002:32 >>,
-    DummyIPbusRequest2_3 = << 16#02030001:32, 16#02030002:32, 16#02030003:32 >>,
-    DummyIPbusRequest3_1 = << 16#03010001:32 >>,
-    DummyIPbusRequest3_2 = << 16#03020001:32, 16#03020002:32 >>,
-    DummyIPbusRequest3_3 = << 16#03030001:32, 16#03030002:32, 16#03030003:32 >>,
+    HwPort1 = lists:nth(1, ?DUMMY_HW_PORTS_LIST),
+    HwPort2 = lists:nth(2, ?DUMMY_HW_PORTS_LIST),
+    HwPort3 = lists:nth(3, ?DUMMY_HW_PORTS_LIST),
+    % The dummy content of our requests - only going to echo server, so no need for real IPbus content.
+    HwPortsRequestsList = [{HwPort1, << 16#01010001:32 >>},
+                           {HwPort2, << 16#02010001:32 >>},
+                           {HwPort3, << 16#03010001:32 >>},
+                           {HwPort1, << 16#01020001:32, 16#01020002:32 >>},
+                           {HwPort2, << 16#02020001:32, 16#02020002:32 >>},
+                           {HwPort3, << 16#03020001:32, 16#03020002:32 >>},
+                           {HwPort1, << 16#01030001:32, 16#01030002:32, 16#01030003:32 >>},
+                           {HwPort2, << 16#02030001:32, 16#02030002:32, 16#02030003:32 >>},
+                           {HwPort3, << 16#03030001:32, 16#03030002:32, 16#03030003:32 >>}],
     % Build the test request
-    TestRequest = << ?LOCALHOST:32,
-                     (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest1_1) div 4):16,
-                     DummyIPbusRequest1_1/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(2, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest2_1) div 4):16,
-                     DummyIPbusRequest2_1/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(3, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest3_1) div 4):16,
-                     DummyIPbusRequest3_1/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest1_2) div 4):16,
-                     DummyIPbusRequest1_2/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(2, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest2_2) div 4):16,
-                     DummyIPbusRequest2_2/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(3, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest3_2) div 4):16,
-                     DummyIPbusRequest3_2/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest1_3) div 4):16,
-                     DummyIPbusRequest1_3/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(2, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest2_3) div 4):16,
-                     DummyIPbusRequest2_3/binary,
-                     ?LOCALHOST:32,
-                     (lists:nth(3, ?DUMMY_HW_PORTS_LIST)):16, (size(DummyIPbusRequest3_3) div 4):16,
-                     DummyIPbusRequest3_3/binary >>,
+    TestRequests = [ << ?LOCALHOST:32,
+                        HwPort:16, (size(Request) div 4):16,
+                        Request/binary >> || {HwPort, Request} <- HwPortsRequestsList ],
     % The response we expect back
-    ExpectedResponse = << (size(DummyIPbusRequest1_1) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest1_1/binary,
-                          (size(DummyIPbusRequest2_1) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(2, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest2_1/binary,
-                          (size(DummyIPbusRequest3_1) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(3, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest3_1/binary,
-                          (size(DummyIPbusRequest1_2) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest1_2/binary,
-                          (size(DummyIPbusRequest2_2) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(2, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest2_2/binary,
-                          (size(DummyIPbusRequest3_2) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(3, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest3_2/binary,
-                          (size(DummyIPbusRequest1_3) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(1, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest1_3/binary,
-                          (size(DummyIPbusRequest2_3) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(2, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest2_3/binary,
-                          (size(DummyIPbusRequest3_3) +8):32,
-                          ?LOCALHOST:32,
-                          (lists:nth(3, ?DUMMY_HW_PORTS_LIST)):16, ?ERRCODE_SUCCESS:16,
-                          DummyIPbusRequest3_3/binary >>,
+    ExpectedResponses = [ << (size(IPbusRequest) +8):32,
+                             ?LOCALHOST:32,
+                             HwPort:16, ?ERRCODE_SUCCESS:16,
+                             IPbusRequest/binary >> || {HwPort, IPbusRequest} <- HwPortsRequestsList ],
     % Run the test
-    ReceivedResponse = create_send_receive(TestBaggage#test_baggage.listen_socket, TestRequest),
-    ?assertEqual(ExpectedResponse, ReceivedResponse).
+    ReceivedResponses = create_send_receive(TestBaggage#test_baggage.listen_socket, TestRequests),
+    ?assertEqual(ExpectedResponses, ReceivedResponses).
 
 
 % Tests to see if the transaction manager ignores requests
@@ -259,23 +206,34 @@ ignore_bad_data_non_integer_number_of_words(TestBaggage) ->
 %%% Test Helper Functions
 %%% ==========================================================================
 
-% Creates the transaction manager under test, sends the test request to it, receives
-% and returns the response binary.  If there is no response before the timeout is
+% Creates the transaction manager under test, sends one or more test requests to it, receives
+% and returns the response binary(s).  If there is no response before the timeout is
 % reached, it returns the atom no_response
-% @spec create_send_receive(TcpListenSocket::socket(), TestRequest::binary) -> Response::binary() | no_response
-create_send_receive(TcpListenSocket, TestRequest) ->
+% @spec create_send_receive(TcpListenSocket::socket(), TestRequests) -> Responses
+%         TestRequests = binary() | [binary()]
+%         Responses    = Response | [Response]
+%         Response     = binary() | no_response
+create_send_receive(TcpListenSocket, TestRequest) when is_binary(TestRequest) ->
+    lists:nth(1, create_send_receive(TcpListenSocket, [TestRequest]) );
+create_send_receive(TcpListenSocket, TestRequests) when is_list(TestRequests) ->
     ch_transaction_manager:start_link(TcpListenSocket),
     % Connect to the transaction manager
     {ok, ClientSocket} = gen_tcp:connect("localhost", ?CONTROL_HUB_TCP_LISTEN_PORT, [binary, {packet, 4}]),
-    ok = gen_tcp:send(ClientSocket, TestRequest),
-    ReceivedResponse = receive
-                           {tcp, ClientSocket, Bin} -> Bin
-                       after 10 ->
-                           no_response
-                       end,
+    ReceivedResponses = [send_receive(ClientSocket, Request) || Request <- TestRequests],
     gen_tcp:close(ClientSocket),
     % Wait for the transaction manager to shut down normally. Can cause problems in debug
     % trace mode otherwise (process gets killed whilst debug stdout is occurring). Need
     % to find a better way around this problem...
     timer:sleep(10),
-    ReceivedResponse.
+    ReceivedResponses.
+
+% Sends a test request to ControlHub, then waits for and returns the response binary.
+% @spec send_receive(Socket::socket(), RequestPacket::binary()) -> Response::binary() | no_response
+send_receive(Socket, RequestPacket) ->
+    ok = gen_tcp:send(Socket, RequestPacket),
+    receive
+        {tcp, Socket, Bin} -> Bin
+    after 10 ->
+        no_response
+    end.
+
