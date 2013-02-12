e3f44a71387143a0df5c4018c09669da8cbe0350
Carlos Ghabrous Larrea <Carlos.Ghabrous@cern.ch>
2013-02-12 14:41:35 +0000

    [CACTUS SVN 8328]

--- uhal/gui/modules/hardware.py
+++ uhal/gui/modules/hardware.py
@@ -4,20 +4,80 @@ import os
 
 import pycohal
 
+class AbstractNode:
 
-class Hardware:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class MemoryNode(AbstractNode):
+
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+
+    
+class RegisterNode(AbstractNode):
+
+    def __init__(self):
+
+        self.id = id
+        self.address = address
+        self.permission = permission
+        self.tag = tag
+        self.mask = mask
+
+    def update(self):
+        pass
+    
+
+
+
+class IPEndPoint(AbstractNode):
+
+    def __init__(self_):
+
+        self.status = status
+        self.uri = uri
+        self.id = id
+        self.address_table = address_table
+        self.nodeList = []
+
+    def update(self):
+        pass
+
+
+        
+
+class Hardware(IPEndPoint):
 
     def __init__(self, connection_file = ""):
 
         self.connection_file = connection_file
-        self.IP_end_points_list = []
+        self.ip_end_points_dict = {}
+        self.hw_manager = pycohal.ConnectionManager(str("file://" + connection_file))
 
-        self._load_hardware(self.connection_file)
+        #how to know whether hw_manager was correctly instantiated?
+        _load_hardware(self.hw_manager)
+        
 
-        print "Hardware class instantiated with file ", self.connection_file
+    def _load_hardware(self, hw_man):
 
+        
 
-    def _load_hardware(self, conn_file):
-        pass
+        
 
+    def update(self):
+
+        for name in self.ip_end_points_dict.keys():
+            self.ip_end_points_dict[name].update()
+
+            
+            
         
