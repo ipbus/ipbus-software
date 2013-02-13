2bf6ac0798a50b7a3a80000218f4adb7743c02f1
Tom Williams <tom.williams@cern.ch>
2013-02-13 11:36:55 +0000

    #TRAC-162 : Set architecture in pycohal rpm name     [CACTUS SVN 8341]

--- uhal/pycohal/setup.py
+++ uhal/pycohal/setup.py
@@ -5,12 +5,12 @@ from os.path import join
 
 CACTUS_ROOT    = os.environ['CACTUS_ROOT']
 INSTALL_PREFIX = os.environ['INSTALL_PREFIX']
-VERSION_STRING = (os.environ['PACKAGE_VER_MAJOR']+'.'+os.environ['PACKAGE_VER_MINOR']+'.'+os.environ['PACKAGE_VER_PATCH']+
-                   '_python'+str(sys.version_info[0])+'.'+str(sys.version_info[1]) )
+PYCOHAL_VERSION = (os.environ['PACKAGE_VER_MAJOR']+'.'+os.environ['PACKAGE_VER_MINOR']+'.'+os.environ['PACKAGE_VER_PATCH'])
+PYTHON_VERSION = str(sys.version_info[0])+'.'+str(sys.version_info[1])+'.'+str(sys.version_info[2])
 
 
 setup(name='cactuscore-uhal-pycohal',
-      version = VERSION_STRING,
+      version = PYCOHAL_VERSION+"_python"+PYTHON_VERSION,
       description = 'Python bindings for the CACTUS uhal libraries.',
       author = 'Tom Williams', 
       author_email = 'T.Williams@cern.ch',
