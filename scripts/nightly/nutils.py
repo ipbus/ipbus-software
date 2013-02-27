4191aa1ea5f8e02e43d46301d4132a22a6def083
CMS L1 trigger online software service account <>
2013-02-27 09:54:16 +0000

    #TRAC-182 * Unbuffered subprocess call and python call * Use independent queue     [CACTUS SVN 8600]

--- scripts/nightly/nutils.py
+++ scripts/nightly/nutils.py
@@ -3,8 +3,9 @@ import sys
 import os
 import logging
 import subprocess
-import fcntl
 import time
+import Queue
+import threading
 
 #maximum seconds without logs
 SOFT_TIMEOUT_S = 10*60
@@ -17,7 +18,11 @@ def report_error(msg,exception,log):
     elif log:
         global logger
         logger.error(msg)
-        
+
+def reader(f,q,log=True):
+    for line in iter(f.readline,""):
+        q.put(line)
+
 def system(cmd, exception = True, log=True):
     '''Execute shell comman    
     '''
@@ -25,43 +30,36 @@ def system(cmd, exception = True, log=True):
     if log:
         logger.info("%s..." % cmd)
         
-    p  = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,stdin=None,bufsize=0,shell=True)
-    fl = fcntl.fcntl(p.stdout, fcntl.F_GETFL)
-    fcntl.fcntl(p.stdout, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-
-    content = ""
-    
+    p  = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,stdin=None,shell=True)
+    q = Queue.Queue()
+    t = threading.Thread(target=reader, args=(p.stdout, q,log))
+    t.setDaemon(True)
+    t.start() 
+   
     start = time.time()
-    last = start
-    current = start
+    content = ""
     while True:
+        current = time.time()
         try:
-            nextline = filter(lambda x: x in string.printable,p.stdout.readline())
-            content = content + nextline
-            sys.stdout.write(nextline)
-            sys.stdout.flush()
-            last = time.time()
-        except IOError:
-            time.sleep(0.1)
+            l = q.get(True,SOFT_TIMEOUT_S)
+            if log:
+                print l,
+            content = content + l
+        except Queue.Empty:
+            
+            report_error("ERROR: %s (unresponsive command, missing output for %d sec)" % (cmd,SOFT_TIMEOUT_S),exception,log)
+            return (content,-1)
 
-            current = time.time()    
-            if (current-start) > HARD_TIMEOUT_S:
-                system("kill -9 %d" % p.pid,log=False,exception=False)
-                report_error("ERROR: %s (command too slow, timeout of %d sec)" % (cmd,HARD_TIMEOUT_S),exception,log)
-                return (content,-9)
-            
-            if (current-last) > SOFT_TIMEOUT_S:
-                system("kill -9 %d" % p.pid,log=False,exception=False)
-                report_error("ERROR: %s (unresponsive command, missing output for %d sec)" % (cmd,SOFT_TIMEOUT_S),exception,log)
-                return (content,-9)
+        print current-start
+        if p.poll()!= None:
+            break
+        elif (current-start) > HARD_TIMEOUT_S:
+            report_error("ERROR: %s (command too slow, timeout of %d sec)" % (cmd,HARD_TIMEOUT_S),exception,log)
+            return (content,-1)
                 
-            if p.poll()!= None:
-                break
-            
     if p.poll():
         report_error("ERROR: %s (error=%s)" % (cmd, p.poll()),exception,log)
 
-    
     return (content, p.poll())
 
 
