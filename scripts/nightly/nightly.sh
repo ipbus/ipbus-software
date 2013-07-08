0342a48f253ebf0bec0c348cb57b5cacdb62d047
CMS L1 trigger online software service account <>
2013-07-08 15:46:56 +0000

    #TRAC-300 : Changing nightly.sh to run nightlies from home dir.     [CACTUS SVN 21536]

--- scripts/nightly/nightly.sh
+++ scripts/nightly/nightly.sh
@@ -3,14 +3,15 @@
 #Nightly bootstrap script to update the nightly script automatically
 
 #Update nightly scripts
-cd $HOME/nightly
-svn up
+cd $HOME
+svn up nightly
 
 #Create directory to store logs
 PLATFORM=`python -c "import platform;print platform.platform()"`
-LOG_DIR=$HOME/www/nightly/${!#}/$PLATFORM/logs
+CONF_FILE_NAME=`basename $1`
+LOG_DIR=$HOME/www/nightly/$CONF_FILE_NAME/$PLATFORM/logs
 echo $LOG_DIR
 mkdir -p $LOG_DIR
 
 #execute nightly
-python nightly.py $@ 2>&1 | tee $LOG_DIR/nightly.log
+python nightly/nightly.py $@ 2>&1 | tee $LOG_DIR/nightly.log
