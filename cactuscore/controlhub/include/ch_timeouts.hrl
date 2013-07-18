e18ca4708ded13762db1e7a8069cf3e83b788e94
Tom Williams <tom.williams@cern.ch>
2013-07-18 21:05:40 +0000

    controlhub: Effectively disable controlhub's internal timeout to help uHAL debugging.     [CACTUS SVN 21791]

--- cactuscore/controlhub/include/ch_timeouts.hrl
+++ cactuscore/controlhub/include/ch_timeouts.hrl
@@ -15,7 +15,7 @@
 % process.  If this timeout is reached, it would generally imply that the
 % device client in question has died, or it's so heavily backlogged with
 % requests that it's failed to respond in time.
--define(RESPONSE_FROM_DEVICE_CLIENT_TIMEOUT, 600).
+-define(RESPONSE_FROM_DEVICE_CLIENT_TIMEOUT, 600000).
 
 % The time (ms) in which a device client process will shut down if not communicating with board
 -define(DEVICE_CLIENT_SHUTDOWN_AFTER, 15000).
