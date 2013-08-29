a2e2c5c25f79b72469a67f3144ce25c2dc03ffb3
Andrew William Rose <andrew.william.rose@cern.ch>
2013-08-29 16:53:40 +0000

    Add files missed in last commit     [CACTUS SVN 22885]

---
+++ cactuscore/uhal/log/include/uhal/log/LogLevels.hpp
@@ -0,0 +1,166 @@
+
+#ifndef _loglevels_hpp_
+#define _loglevels_hpp_
+
+#include <iostream>
+
+
+
+
+template< typename U>
+void insert ( std::ostream& aStr , const U& aU )
+{
+  aStr << aU;
+}
+
+
+__attribute__ ( ( warning ( "Insertion of integer types can result in implicit casts. Consider using the Integer() formatter instead" ) ) )
+void insert ( std::ostream& aStr , const uint32_t& aUint ) ;
+
+__attribute__ ( ( warning ( "Insertion of integer types can result in implicit casts. Consider using the Integer() formatter instead" ) ) )
+void insert ( std::ostream& aStr , const int32_t& aInt );
+
+__attribute__ ( ( warning ( "Insertion of boolean types can result in implicit casts. Consider using the Boolean() formatter instead" ) ) )
+void insert ( std::ostream& aStr , const bool& aBool );
+
+namespace uhal
+{
+
+  template< typename T >
+  class BaseLogLevel
+  {
+    protected:
+      typedef void ( *fPtr ) ( std::ostream& aStr );
+
+      BaseLogLevel ( std::ostream& aStr , fPtr aHeadFunction , fPtr aTailFunction ) : mStr ( aStr ) , mHeadFunction ( aHeadFunction ) , mTailFunction ( aTailFunction )
+      {}
+
+    public:
+      T& operator() ()
+      {
+        return static_cast<T&> ( *this );
+      }
+
+      T& head()
+      {
+        mHeadFunction ( mStr );
+        return static_cast<T&> ( *this );
+      }
+
+      T& tail()
+      {
+        mTailFunction ( mStr );
+        return static_cast<T&> ( *this );
+      }
+
+      std::ostream& stream()
+      {
+        return mStr;
+      }
+
+    private:
+      std::ostream& mStr;
+      fPtr mHeadFunction;
+      fPtr mTailFunction;
+
+  };
+
+
+
+
+  //! Helper struct representing the Fatal log level to allow us to specialize functions according to their log level
+  class FatalLevel : public BaseLogLevel< FatalLevel >
+  {
+      typedef BaseLogLevel< FatalLevel > Base;
+
+    public:
+      FatalLevel ( std::ostream& aStr = std::cout , Base::fPtr aHeadFunction = FatalLevel::colour_head, Base::fPtr aTailFunction = FatalLevel::colour_tail );
+
+      static void colour_head ( std::ostream& aStr );
+      static void colour_tail ( std::ostream& aStr );
+  };
+
+  extern FatalLevel Fatal;
+
+
+  //! Helper struct representing the Error log level to allow us to specialize functions according to their log level
+  class ErrorLevel : public BaseLogLevel< ErrorLevel >
+  {
+      typedef BaseLogLevel< ErrorLevel > Base;
+
+    public:
+      ErrorLevel ( std::ostream& aStr = std::cout , Base::fPtr aHeadFunction = ErrorLevel::colour_head, Base::fPtr aTailFunction = ErrorLevel::colour_tail );
+
+      static void colour_head ( std::ostream& aStr );
+      static void colour_tail ( std::ostream& aStr );
+  };
+
+  extern ErrorLevel Error;
+
+
+
+  //! Helper struct representing the Warning log level to allow us to specialize functions according to their log level
+  class WarningLevel : public BaseLogLevel< WarningLevel >
+  {
+      typedef BaseLogLevel< WarningLevel > Base;
+
+    public:
+      WarningLevel ( std::ostream& aStr = std::cout , Base::fPtr aHeadFunction = WarningLevel::colour_head, Base::fPtr aTailFunction = WarningLevel::colour_tail );
+
+      static void colour_head ( std::ostream& aStr );
+      static void colour_tail ( std::ostream& aStr );
+  };
+
+  extern WarningLevel Warning;
+
+  //! Helper struct representing the Notice log level to allow us to specialize functions according to their log level
+  class NoticeLevel : public BaseLogLevel< NoticeLevel >
+  {
+
+      typedef BaseLogLevel< NoticeLevel > Base;
+
+    public:
+      NoticeLevel ( std::ostream& aStr = std::cout , Base::fPtr aHeadFunction = NoticeLevel::colour_head, Base::fPtr aTailFunction = NoticeLevel::colour_tail );
+
+      static void colour_head ( std::ostream& aStr );
+      static void colour_tail ( std::ostream& aStr );
+  };
+
+  extern NoticeLevel Notice;
+
+  //! Helper struct representing the Info log level to allow us to specialize functions according to their log level
+  class InfoLevel : public BaseLogLevel< InfoLevel >
+  {
+      typedef BaseLogLevel< InfoLevel > Base;
+
+    public:
+      InfoLevel ( std::ostream& aStr = std::cout , Base::fPtr aHeadFunction = InfoLevel::colour_head, Base::fPtr aTailFunction = InfoLevel::colour_tail );
+
+      static void colour_head ( std::ostream& aStr );
+      static void colour_tail ( std::ostream& aStr );
+  };
+
+  extern InfoLevel Info;
+
+  //! Helper struct representing the Debug log level to allow us to specialize functions according to their log level
+  class DebugLevel : public BaseLogLevel< DebugLevel >
+  {
+      typedef BaseLogLevel< DebugLevel > Base;
+
+    public:
+      DebugLevel ( std::ostream& aStr = std::cout , Base::fPtr aHeadFunction = DebugLevel::colour_head, Base::fPtr aTailFunction = DebugLevel::colour_tail );
+
+      static void colour_head ( std::ostream& aStr );
+      static void colour_tail ( std::ostream& aStr );
+  };
+
+  extern DebugLevel Debug;
+
+
+
+}
+
+
+
+#endif
+
---
+++ cactuscore/uhal/log/src/common/LogLevels.cpp
@@ -0,0 +1,139 @@
+#include <uhal/log/LogLevels.hpp>
+
+#include <uhal/log/log_inserters.time.hpp>
+#include <uhal/log/log_inserters.threadID.hpp>
+
+
+void insert ( std::ostream& aStr , const uint32_t& aUint )
+{
+  aStr << aUint;
+}
+
+void insert ( std::ostream& aStr , const int32_t& aInt )
+{
+  aStr << aInt;
+}
+
+void insert ( std::ostream& aStr , const bool& aBool )
+{
+  aStr << aBool;
+}
+
+namespace uhal
+{
+
+  FatalLevel::FatalLevel ( std::ostream& aStr , Base::fPtr aHeadFunction, Base::fPtr aTailFunction ) : Base ( aStr , aHeadFunction , aTailFunction ) {}
+
+  void FatalLevel::colour_head ( std::ostream& aStr )
+  {
+    aStr << "\033[0;31m" //standard red
+         << Time< day,'-',mth,'-',yr,' ',hr,':',min,':',sec,'.',usec > ( Now() )
+         << " [" << ThisThreadID() << "]"
+         << " FATAL - ";
+  }
+
+  void FatalLevel::colour_tail ( std::ostream& aStr )
+  {
+    aStr << "\033[0m" << std::endl;
+  }
+
+  FatalLevel Fatal;
+
+
+  ErrorLevel::ErrorLevel ( std::ostream& aStr, Base::fPtr aHeadFunction, Base::fPtr aTailFunction ) : Base ( aStr , aHeadFunction , aTailFunction ) {}
+
+  void ErrorLevel::colour_head ( std::ostream& aStr )
+  {
+    aStr << "\033[0;31m" //standard red
+         << Time< day,'-',mth,'-',yr,' ',hr,':',min,':',sec,'.',usec > ( Now() )
+         << " [" << ThisThreadID() << "]"
+         << " ERROR - ";
+  }
+
+  void ErrorLevel::colour_tail ( std::ostream& aStr )
+  {
+    aStr << "\033[0m" << std::endl;
+  }
+
+
+  ErrorLevel Error;
+
+
+  WarningLevel::WarningLevel ( std::ostream& aStr, Base::fPtr aHeadFunction, Base::fPtr aTailFunction )  : Base ( aStr , aHeadFunction , aTailFunction ) {}
+
+  void WarningLevel::colour_head ( std::ostream& aStr )
+  {
+    aStr << "\033[0;33m" //standard yellow
+         << Time< day,'-',mth,'-',yr,' ',hr,':',min,':',sec,'.',usec > ( Now() )
+         << " [" << ThisThreadID() << "]"
+         << " WARNING - ";
+  }
+
+  void WarningLevel::colour_tail ( std::ostream& aStr )
+  {
+    aStr << "\033[0m" << std::endl;
+  }
+
+  WarningLevel Warning;
+
+  NoticeLevel::NoticeLevel ( std::ostream& aStr, Base::fPtr aHeadFunction, Base::fPtr aTailFunction )  : Base ( aStr , aHeadFunction , aTailFunction ) {}
+
+  void NoticeLevel::colour_head ( std::ostream& aStr )
+  {
+    aStr << "\033[0;32m" //standard green
+         << Time< day,'-',mth,'-',yr,' ',hr,':',min,':',sec,'.',usec > ( Now() )
+         << " [" << ThisThreadID() << "]"
+         << " NOTICE - ";
+  }
+
+  void NoticeLevel::colour_tail ( std::ostream& aStr )
+  {
+    aStr << "\033[0m" << std::endl;
+  }
+
+  NoticeLevel Notice;
+
+
+  InfoLevel::InfoLevel ( std::ostream& aStr, Base::fPtr aHeadFunction, Base::fPtr aTailFunction ) : Base ( aStr , aHeadFunction , aTailFunction ) {}
+
+  void InfoLevel::colour_head ( std::ostream& aStr )
+  {
+    aStr << "\033[0;36m" //standard cyan
+         << Time< day,'-',mth,'-',yr,' ',hr,':',min,':',sec,'.',usec > ( Now() )
+         << " [" << ThisThreadID() << "]"
+         << " INFO - ";
+  }
+
+  void InfoLevel::colour_tail ( std::ostream& aStr )
+  {
+    aStr << "\033[0m" << std::endl;
+  }
+
+  InfoLevel Info;
+
+
+  DebugLevel::DebugLevel ( std::ostream& aStr, Base::fPtr aHeadFunction, Base::fPtr aTailFunction )  : Base ( aStr , aHeadFunction , aTailFunction ) {}
+
+  void DebugLevel::colour_head ( std::ostream& aStr )
+  {
+    aStr << "\033[0;34m" //standard blue
+         << Time< day,'-',mth,'-',yr,' ',hr,':',min,':',sec,'.',usec > ( Now() )
+         << " [" << ThisThreadID() << "]"
+         << " DEBUG - ";
+  }
+
+  void DebugLevel::colour_tail ( std::ostream& aStr )
+  {
+    aStr << "\033[0m" << std::endl;
+  }
+
+
+  DebugLevel Debug;
+
+
+
+}
+
+
+
+
