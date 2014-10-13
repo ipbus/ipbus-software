9fb5583aec7cc0d6d9f61f24cab342cddd126eaa
CMS L1 trigger online software service account <>
2014-10-13 08:12:05 +0000

    Committing last changes; refs #TRAC-821     [CACTUS SVN 31739]

--- scripts/nightly/nightly.py
+++ scripts/nightly/nightly.py
@@ -11,12 +11,13 @@ options:
    -s, --silent              Do not create the final report and notifications (just logging)
    -l, --list                Print the list of available commands in the given configuration file
    
+   --group=<group>           Choose which group of nightly build targets to execute. 
+                             Available options are: TS_DEV, SUBSYSTEM_DEV, SUBSYSTEM_904
    --commands=<list>         Provide list of commands to execute. If empty, the default set of 
                              commands for the nightly builds will be executed
    --emailto=<email>         Where to send the email report (-s will override this)
    --checkout_as=<username>  Use a different username to checkout the code than the current user
    --build_home=<path>       Specify a custom path to use as build home                     
-   
 """
 
 from nutils import logger, log_setup, system
@@ -26,31 +27,33 @@ import getopt
 import nanalyzer
 import getpass
 
-def execute():
-    logger.info("The following set of commands is available for execution: %s" % ",".join([s for s,c in CONF.COMMANDS]))
+def execute(this_nightly):
+    logger.info("The following set of commands is available for execution: %s" % ",".join([s for s,c in this_nightly.COMMANDS]))
     logger.info("The following set of commands will be executed: %s" % ",".join(commandsToRun))
     
     if ( "CLEANUP_WWW_AREA" in commandsToRun ) :
       logger.info("Cleaning up logs area...")
-      CONF.cleanupLogs()
-    for section,cmds in CONF.COMMANDS:
-      if section in commandsToRun :
-        logger.info("----++++ %s ++++----" % section)
-        for cmd in cmds:
-            system(cmd,log=True, exception=False)
+      this_nightly.cleanupLogs()
+    for command in commandsToRun :
+      for section,cmds in this_nightly.COMMANDS:
+        if section == command : 
+          logger.info("----++++ %s ++++----" % section)
+          for cmd in cmds:
+              system(cmd,log=True, exception=False)
 
 if __name__== "__main__":
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "shl", ["silent", "help", "list", "commands=", "emailto=", "checkout_as=", "build_home="])
+        opts, args = getopt.getopt(sys.argv[1:], "shl", ["silent", "help", "list", "group=", "commands=", "emailto=", "checkout_as=", "build_home="])
     except getopt.GetoptError, err:
         print __doc__
         sys.exit(2)
 
     global commandsToRun
-    commandsViaParam = ""
+    commands_via_param = ""
     
     silent = False
     listonly = False
+    nightly_group = False
     USER_NAME = getpass.getuser()
     checkout_as = USER_NAME
     to_email = USER_NAME +  "@cern.ch"
@@ -65,61 +68,66 @@ if __name__== "__main__":
         if o in ("-l", "--list"):
             listonly=True
         if o in ("--commands"):
-            commandsViaParam=a.split(",")
+            commands_via_param=a.split(",")
         if o in ("--emailto"):
             to_email=a
         if o in ("--checkout_as"):
             checkout_as=a
         if o in ("--build_home"):
             build_home=a
+        if o in ("--group"):
+            nightly_group=a
             
     if len(args) == 1:
-        global CONF
         try:
             p,fn = os.path.split(args[0])
             n,ext = os.path.splitext(fn)
             CONF = __import__(n)
+            
+            if n == "ts_dev2" and nightly_group not in ("TS_DEV","SUBSYSTEM_904","SUBSYSTEM_DEV"):
+              sys.stderr.write("ERROR: Need to define group to run on\n\n")
+              print __doc__
+              sys.exit(2)
+              
+            if n == "uhal_config2" : 
+              nightly_group="UHAL"
+              
+            this_nightly = CONF.Nightly(nightly_group, to_email, checkout_as, build_home)
+            
+
             
         except ImportError,e:
             sys.stderr.write("ERROR: Failed to import '%s': %s\n\n" % (args[0],str(e)))
     else:
         sys.stderr.write("ERROR: Wrong number of arguments\n\n")
         print __doc__
-        sys.exit(2)
-    
-    CONF.TO_EMAIL = to_email
-    CONF.CHECKOUT_NAME = checkout_as
-    CONF.BUILD_HOME = build_home
-
-    # we need to importCommands separately as some paths depend on the
-    # variables set above
-    CONF.importCommands()
-    
+        sys.exit(2)        
+          
     # If no command list is given, use the default set
-    if not commandsViaParam :
-      commandsToRun = CONF.DEFAULT_COMMANDS
+    if not commands_via_param :
+      commandsToRun = this_nightly.DEFAULT_COMMANDS
     else:
-      commandsToRun = commandsViaParam
+      commandsToRun = commands_via_param
       
     if listonly :
-      print "The following set of commands is available for execution: %s" % ",".join([s for s,c in CONF.COMMANDS])
+      print "The following set of commands is available for execution: %s" % ",".join([s for s,c in this_nightly.COMMANDS])
       sys.exit(0)
 
     # Check if command list given is valid
     for command in commandsToRun:
-      if command in [s for s,c in CONF.COMMANDS] :
+      if command in [s for s,c in this_nightly.COMMANDS] :
         continue
       else:
         sys.stderr.write( "ERROR: Command '" + command + "' is not available.\n" )
-        sys.stderr.write( "List of available commands : %s\n" % str([s for s,c in CONF.COMMANDS]) )
+        sys.stderr.write( "List of available commands : %s\n" % str([s for s,c in this_nightly.COMMANDS]) )
         sys.exit(2)
     
     if not silent :
-      logger.info("Reports will be sent to: %s" % CONF.TO_EMAIL )
+      logger.info("Reports will be sent to: %s" % this_nightly.TO_EMAIL )
     
     # Execute build/test/etc.
     try:
-        execute()
+        execute(this_nightly)
     except KeyboardInterrupt,e:
         logger.warning('Aborting after CTRL-C...\n')
         sys.exit(1)
@@ -131,6 +139,8 @@ if __name__== "__main__":
         logger.info("Final reporting and email notifications were disabled")
     else:
         logger.info("----++++REPORTING++++----")
-        nanalyzer.report(CONF)
-        system("mkdir -p %s" % CONF.NIGHTLY_LOG_DIR,log=True, exception=False)
-        system( "sudo cp -r %s %s" % ("/var/log/*",CONF.NIGHTLY_LOG_DIR),log=True, exception=False)
+        system("mkdir -p %s" % this_nightly.NIGHTLY_LOG_DIR,log=True, exception=False)
+        system("sudo mv %s %s" % ("/tmp/nightly.log",this_nightly.NIGHTLY_LOG_DIR),log=True, exception=False)
+        system("sudo cp -r %s %s" % ("/var/log/*",this_nightly.NIGHTLY_LOG_DIR),log=True, exception=False)
+        nanalyzer.report(this_nightly)
+        
--- scripts/nightly/nightly.sh
+++ scripts/nightly/nightly.sh
@@ -4,13 +4,7 @@
 
 #Update nightly scripts
 cd $HOME/nightly
-svn up
-
-#Create directory to store logs
-PLATFORM=`python -c "import platform;print platform.platform()"`
-LOG_DIR=$HOME/www/nightly/${!#}/$PLATFORM/logs
-echo $LOG_DIR
-mkdir -p $LOG_DIR
+#svn up
 
 #execute nightly
-python nightly.py $@ 2>&1 | tee $LOG_DIR/nightly.log
+python nightly2.py $@ 2>&1 | tee /tmp/nightly.log
