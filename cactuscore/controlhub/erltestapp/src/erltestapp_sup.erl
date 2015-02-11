6f34faa4529a302a33d1d8daa611eba5ec0e8f95
Tom Williams <tom.williams@cern.ch>
2015-02-11 21:54:51 +0000

    controlhub, refs #TRAC-217 : Add dummy process to erltestapp, in order to check all is well     [CACTUS SVN 34318]

--- cactuscore/controlhub/erltestapp/src/erltestapp_sup.erl
+++ cactuscore/controlhub/erltestapp/src/erltestapp_sup.erl
@@ -6,7 +6,7 @@
 -export([start_link/0]).
 
 %% Supervisor callbacks
--export([init/1]).
+-export([init/1,talking_proc_loop/0]).
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
@@ -16,6 +16,7 @@
 %% ===================================================================
 
 start_link() ->
+    Pid = spawn_link(?MODULE,talking_proc_loop,[]),
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 %% ===================================================================
@@ -23,5 +24,12 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
+    Pid = spawn_link(?MODULE,talking_proc_loop,[]),
     {ok, { {one_for_one, 5, 10}, []} }.
 
+
+
+talking_proc_loop() ->
+    timer:sleep(5000),
+    io:format("Hi! this is proc ~p", [self()]),
+    talking_proc_loop().
