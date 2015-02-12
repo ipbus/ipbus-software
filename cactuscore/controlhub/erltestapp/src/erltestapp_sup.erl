c4b546ce85de6fe5bb5ac00f43999adcdb033392
Tom Williams <tom.williams@cern.ch>
2015-02-12 11:14:07 +0000

    controlhub, refs #TRAC-217 : Add lager parse-transform compiler flag to rebar config; store standard Erlang crash reports in dedicated file; make terminal log messages coloured     [CACTUS SVN 34326]

--- cactuscore/controlhub/erltestapp/src/erltestapp_sup.erl
+++ cactuscore/controlhub/erltestapp/src/erltestapp_sup.erl
@@ -1,14 +1,12 @@
 -module(erltestapp_sup).
 
--compile([{parse_transform, lager_transform}]).
-
 -behaviour(supervisor).
 
 %% API
 -export([start_link/0]).
 
 %% Supervisor callbacks
--export([init/1,talking_proc_loop/0]).
+-export([init/1,talking_proc/1]).
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
@@ -18,7 +16,6 @@
 %% ===================================================================
 
 start_link() ->
-    Pid = spawn_link(?MODULE,talking_proc_loop,[]),
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 %% ===================================================================
@@ -26,14 +23,28 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    spawn_link(?MODULE,talking_proc_loop,[]),
-    {ok, { {one_for_one, 5, 10}, []} }.
+    spawn(?MODULE, talking_proc, [3]),
+    spawn(?MODULE, talking_proc, [5]),
+    spawn(?MODULE, talking_proc, [11]),
+    spawn(?MODULE, talking_proc, [23]),
+    {ok,
+      { {one_for_one, 5, 10}, []}
+    }.
 
 
 
-talking_proc_loop() ->
-    timer:sleep(5000),
-    lager:info("Low-priority log message from proc ~p", [self()]),
-    io:format("Hi! this is proc ~p", [self()]),
-    lager:emergency("Super-critical log message from proc ~p", [self()]),
-    talking_proc_loop().
+talking_proc(NrTimes) ->
+    talking_proc_loop(0, NrTimes). 
+
+
+talking_proc_loop(MaxCount, MaxCount) ->
+    lager:info("Proc ~p committing suicide now ...", [self()]),
+    true = false;
+talking_proc_loop(Counter, MaxCount) ->
+    timer:sleep(2000),
+    lager:info("Low-priority log message ~p/~p from proc ~p", [Counter, MaxCount, self()]),
+    timer:sleep(1000),
+    io:format("Hi! this is proc ~p~n", [self()]),
+    timer:sleep(2000),
+    lager:emergency("Super-critical log message ~p/~p from proc ~p", [Counter, MaxCount, self()]),
+    talking_proc_loop(Counter+1, MaxCount).
