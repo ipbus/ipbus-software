3d63bd4540b70b701a9c2eddabd411a83ebd40b4
Tom Williams <tom.williams@cern.ch>
2017-07-02 21:51:22 +0100

    uhal doxygen, refs issue #42 : Re-write doxygen script, removed unused files, and use new script in CI

--- scripts/doxygen/api_dev_uhal.sh
+++ scripts/doxygen/api_dev_uhal.sh
@@ -1,30 +1,66 @@
 #!/bin/bash
 
-export DOXYGEN_HOME=${HOME}/doxygen/doxygen-1.8.6/
-export CACTUS_SANDBOX=/build/cactus/cactus/trunk
-#export CACTUS_SANDBOX=${HOME}/doxygen/test
-export API_DIR=/afs/cern.ch/user/c/cactus/www/nightly/api
+# 0. Check number of arguments
+if [ $# -ne 1 ]; then
+    echo "Incorrect usage"
+    echo "`basename $0` <project name suffix>"
+    exit 1
+fi
+
+
+# 1. Look for doxygen command
+if [ -n "${DOXYGEN_BIN_DIR}" ]; then
+    DOXYGEN_BIN=${DOXYGEN_BIN_DIR}/doxygen
+    if [ -x "${DOXYGEN_BIN}" ]; then
+        echo "Cannot find doxygen exe under directory '${DOXYGEN_DIR}'. Aborting"
+        exit 1
+    fi 
+else
+	command -v doxygen >/dev/null 2>&1 || { echo "Cannot find doxygen command. Aborting." >&2; exit 1; }
+    DOXYGEN_BIN="doxygen"
+fi
+echo "Using doxygen command: ${DOXYGEN_BIN}"
+echo "   (version `doxygen --version`)"
+
+
+# 2. Set environment variables for doxygen
+export REPO_BASE_DIR=$( readlink -f $(dirname $BASH_SOURCE)/../../ )
+
+export DOXYGEN_PROJECT_NAME="&mu;HAL $1" # '&mu;HAL (nightly)'
+export DOXYGEN_PROJECT_LOGO="${REPO_BASE_DIR}/scripts/doxygen/cactus_logo.png"
+export DOXYGEN_HTML_HEADER="${REPO_BASE_DIR}/scripts/doxygen/header.html"
+export DOXYGEN_HTML_EXTRA_FILES="${REPO_BASE_DIR}/scripts/doxygen/favicon.ico"
+export DOXYGEN_MAINPAGE="${REPO_BASE_DIR}/cactuscore/uhal/README.md"
+export DOXYGEN_INPUTS="${REPO_BASE_DIR}/cactuscore/uhal "
+export DOXYGEN_EXCLUDE_PATTERNS=''
 export DOXYGEN_OUTPUT=/tmp/api_uhal
-export DOXYGEN_WWW=${API_DIR}/html_dev_uhal
 
-cd ${HOME}/nightly/doxygen
-echo "Cleaning up target directory"
-rm -r ${DOXYGEN_OUTPUT}/html
+echo "Input parameters ..."
+echo "  DOXYGEN_PROJECT_NAME     = ${DOXYGEN_PROJECT_NAME}"
+echo "  DOXYGEN_PROJECT_LOGO     = ${DOXYGEN_PROJECT_LOGO}"
+echo "  DOXYGEN_HTML_HEADER      = ${DOXYGEN_HTML_HEADER}"
+echo "  DOXYGEN_HTML_EXTRA_FILES = ${DOXYGEN_HTML_EXTRA_FILES}"
+echo "  DOXYGEN_MAINPAGE         = ${DOXYGEN_MAINPAGE}"
+echo "  DOXYGEN_INPUTS           = ${DOXYGEN_INPUTS}"
+echo "  DOXYGEN_EXCLUDE_PATTERNS = ${DOXYGEN_EXCLUDE_PATTERNS}"
+echo "  DOXYGEN_OUTPUT           = ${DOXYGEN_OUTPUT}"
 
-DOXYGEN_MAINPAGE="${CACTUS_SANDBOX}/cactuscore/uhal/README.md"
-DOXYGEN_INPUTS="${CACTUS_SANDBOX}/cactuscore/uhal "
-DOXYGEN_PROJECT_NAME='&mu;HAL (nightly)'
-DOXYGEN_EXCLUDE_PATTERNS=''
-
-echo DOXYGEN_INPUTS=${DOXYGEN_INPUTS}
-export DOXYGEN_MAINPAGE DOXYGEN_INPUTS DOXYGEN_PROJECT_NAME
-${DOXYGEN_HOME}/bin/doxygen cactus-v3.doxy
+echo "Cleaning up target directory, ${DOXYGEN_OUTPUT}"
+rm -rf ${DOXYGEN_OUTPUT}/html
 
 
-echo "Removing old APIs"
-rm -r ${DOXYGEN_WWW}
+# 3. Main command
+${DOXYGEN_BIN} ${REPO_BASE_DIR}/scripts/doxygen/cactus-v3.doxy
 
-echo "Uploading..."
-mkdir -p ${API_DIR}
-cp -a ${DOXYGEN_OUTPUT}/html ${DOXYGEN_WWW}
-echo "Done"
+
+# 4. Copy output to ${TARGET_DOXY_DIR} if specified
+if [ -n "${TARGET_DOXY_DIR}" ]; then
+    echo "Copying output to TARGET_DOXY_DIR=${TARGET_DOXY_DIR} ..."
+    echo "(Removing old copy first)"
+    rm -r ${TARGET_DOXY_DIR}
+    mkdir -p ${TARGET_DOXY_DIR}
+    cp -a ${DOXYGEN_OUTPUT}/html ${TARGET_DOXY_DIR}
+    echo "Done"
+else
+    echo "doxygen output stored in ${DOXYGEN_OUTPUT}"
+fi
