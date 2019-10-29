f92c92d797750ea909e2e4b8aabac6462ddd0320
Dan Gastler <dgastler@gmail.com>
2019-10-29 19:06:35 -0400

    Fix for SIGBUS

--- copylibs.sh
+++ copylibs.sh
@@ -3,6 +3,6 @@ LIBLOG=uhal/log/lib/*.2.6
 LIBGRAMMAR=uhal/grammars/lib/*.2.6
 LIBPUGI=extern/pugixml/pugixml-1.2/libpugixml.so
 
-APOLLO=apollo:/work/lib/
+APOLLO=root@192.168.30.52:/work/lib/
 
 scp ${LIBUHAL} ${LIBLOG} ${LIBGRAMMAR} ${LIBPUGI} ${APOLLO}
