27817f391e5fc3a49147947b2869191b8b9586dd
Dan Gastler <dgastler@gmail.com>
2019-10-31 12:49:53 -0400

    Updated license to be correct, hopefully

--- uhal/uhal/src/common/ProtocolUIO.cpp
+++ uhal/uhal/src/common/ProtocolUIO.cpp
@@ -1,14 +1,43 @@
 /*
 ---------------------------------------------------------------------------
-This is an extension of uHAL to directly access AXI slaves via the linux
-UIO driver.
 
-Dan Gastler, Boston University
-email dgastler <AT> bu.edu
+    This is an extension of uHAL to directly access AXI slaves via the linux
+    UIO driver. 
+
+    This file is part of uHAL.
+
+    uHAL is a hardware access library and programming framework
+    originally developed for upgrades of the Level-1 trigger of the CMS
+    experiment at CERN.
+
+    uHAL is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    uHAL is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with uHAL.  If not, see <http://www.gnu.org/licenses/>.
+
+
+      Andrew Rose, Imperial College, London
+      email: awr01 <AT> imperial.ac.uk
+
+      Marc Magrans de Abril, CERN
+      email: marc.magrans.de.abril <AT> cern.ch
+
+      Tom Williams, Rutherford Appleton Laboratory, Oxfordshire
+      email: tom.williams <AT> cern.ch
+
+      Dan Gastler, Boston University 
+      email: dgastler <AT> bu.edu
+      
 ---------------------------------------------------------------------------
- */
-
-
+*/
 /**
 	@file
 	@author Siqi Yuan / Dan Gastler / Theron Jasper Tarigo
