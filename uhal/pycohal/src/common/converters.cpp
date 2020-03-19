763826f4365d71c27c65ecb5b495b006364dd2b1
Tom Williams <tom.williams@cern.ch>
2020-03-19 18:32:08 +0000

    uHAL: Check whether to-/from-Python converters already registered before registering them. Fixes #191

--- uhal/pycohal/src/common/converters.cpp
+++ uhal/pycohal/src/common/converters.cpp
@@ -10,12 +10,25 @@ namespace bpy = boost::python ;
 
 void pycohal::register_converters()
 {
-  pycohal::Converter_std_vector_from_list<uint32_t>();
-  pycohal::Converter_std_vector_from_list<std::string>();
-  bpy::to_python_converter< std::vector<std::string>, pycohal::Converter_std_vector_to_list<std::string> >();
-  bpy::to_python_converter< std::vector<uint32_t>, pycohal::Converter_std_vector_to_list<uint32_t> >();
-  bpy::to_python_converter< boost::unordered_map<std::string, std::string>, pycohal::Converter_boost_unorderedmap_to_dict<std::string,std::string> >(); 
+  const bpy::converter::registration* reg;
 
+  reg = bpy::converter::registry::query(bpy::type_id< std::vector<uint32_t> >());
+  if (reg == NULL or (*reg).m_to_python == NULL)
+  {
+    pycohal::Converter_std_vector_from_list<uint32_t>();
+    bpy::to_python_converter< std::vector<uint32_t>, pycohal::Converter_std_vector_to_list<uint32_t> >();
+  }
+
+  reg = bpy::converter::registry::query(bpy::type_id< std::vector<std::string> >());
+  if (reg == NULL or (*reg).m_to_python == NULL)
+  {
+    pycohal::Converter_std_vector_from_list<std::string>();
+    bpy::to_python_converter< std::vector<std::string>, pycohal::Converter_std_vector_to_list<std::string> >();
+  }
+
+  reg = bpy::converter::registry::query(bpy::type_id< boost::unordered_map<std::string, std::string> >());
+  if (reg == NULL or (*reg).m_to_python == NULL)
+    bpy::to_python_converter< boost::unordered_map<std::string, std::string>, pycohal::Converter_boost_unorderedmap_to_dict<std::string,std::string> >();
 }
 
 
