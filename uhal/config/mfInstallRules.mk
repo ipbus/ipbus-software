9f1255b5a5b57ee83595ad21229bfde37c2c6c5c
Tom Williams <tom.williams@cern.ch>
2020-06-12 00:00:28 +0100

    uHAL: Use relative symlinks in 'make install'. Fixes #202

--- uhal/config/mfInstallRules.mk
+++ uhal/config/mfInstallRules.mk
@@ -7,7 +7,7 @@ install:
 	if [ -d bin     ]; then cd bin    ; find . -type f -exec install -D -m 755 {} ${bindir}/${Project}/{} \; ; fi
 	if [ -d scripts ]; then cd scripts; find . -type f -exec install -D -m 755 {} ${bindir}/${Project}/{} \; ; fi
 	if [ -d lib     ]; then cd lib    ; find . -type f -exec install -D -m 755 {} ${libdir}/{} \; ; fi
-	if [ -d lib     ]; then cd lib    ; find . -type l -exec sh -c 'ln -s -f ${libdir}/$$(basename $$(readlink $$0)) ${libdir}/$$0' {} \; ; fi
+	if [ -d lib     ]; then cd lib    ; find . -type l -exec sh -c 'ln -s -f $$(basename $$(readlink $$0)) ${libdir}/$$0' {} \; ; fi
 	if [ -d include ]; then cd include; find . -type f -exec install -D -m 644 {} ${includedir}/{} \; ; fi
 	if [ -d etc     ]; then cd etc    ; find . -type f -exec install -D -m 644 {} ${sysconfdir}/{} \; ; fi
 
