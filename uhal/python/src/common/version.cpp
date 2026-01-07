1a424a9341124bfd1a1fd3a9bf764a1ca76f5541
Tom Williams <tom.williams@cern.ch>
2026-01-07 17:20:01 +0000

    Add missing boost::optional and boost::variant to-Python converters, for new build info

--- uhal/python/src/common/version.cpp
+++ uhal/python/src/common/version.cpp
@@ -1,8 +1,34 @@
 #include "uhal/pycohal/version.hpp"
 
+#include "pybind11/stl.h" // Automatically adds converters for STL collection/map classes
 #include "pybind11/pybind11.h"
 
 #include "uhal/version.hpp"
+
+
+// Define `boost::variant` type caster, for C++ to Python conversion.
+// Copied from pybind11 docs - https://pybind11.readthedocs.io/en/stable/advanced/cast/stl.html
+namespace PYBIND11_NAMESPACE { namespace detail {
+    template <typename... Ts>
+    struct type_caster<boost::variant<Ts...>> : variant_caster<boost::variant<Ts...>> {};
+
+    // Specifies the function used to visit the variant -- `apply_visitor` instead of `visit`
+    template <>
+    struct visit_helper<boost::variant> {
+        template <typename... Args>
+        static auto call(Args &&...args) -> decltype(boost::apply_visitor(args...)) {
+            return boost::apply_visitor(args...);
+        }
+    };
+}} // namespace PYBIND11_NAMESPACE::detail
+
+// Define optional-type type_caster for boost::optional
+namespace pybind11 {
+namespace detail {
+template <typename T>
+struct type_caster<boost::optional<T>> : optional_caster<boost::optional<T>> {};
+}
+}
 
 
 namespace py = pybind11;
